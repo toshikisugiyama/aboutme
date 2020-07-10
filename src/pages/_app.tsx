d5c62a25de7a7b9568e59370357311ed0c07b524
sugiyamatoshiki <s.toshiki.llwz8869@gmail.com>
2020-07-10 15:26:13 +0900

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import 'modern-css-reset'
+import '../styles/styles.scss'
+const App = ({ Component, pageProps }: { Component: any, pageProps: any }) => (
+  <Component {...pageProps} />
+)
+
+export default App
---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>About Me</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="container">
+
+      </main>
+
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
